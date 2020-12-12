5cc2112c5a0aafc1e8fe299e992efc2c5604893f
dunsan2000 <dunsan2000@naver.com>
2020-12-12 16:48:27 +0900

    발사버튼 추가 및 동작 코드 구현

--- GithubSample/Form1.Designer.cs
+++ GithubSample/Form1.Designer.cs
@@ -29,13 +29,37 @@ namespace GithubSample
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.Font = new System.Drawing.Font("굴림", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button1.Location = new System.Drawing.Point(64, 47);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(479, 303);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "발사!";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(604, 406);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
 
